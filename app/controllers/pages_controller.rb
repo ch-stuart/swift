80d3611c52c8e9711fdce760b68535533e245b09
cs <cs@enure.net>
2010-12-03 22:22:27 -0800

    basic authentication
    add yui reset css
    
    basic styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
     
+  before_filter :authenticate, :except => [ :index, :home, :show ]
+    
   def home
     render :text => "swift industries"
   end
