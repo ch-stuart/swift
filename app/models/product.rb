95ff1d267ba5421c36b4e237bd27143889b6e10d
cs <cs@enure.net>
2011-01-15 14:31:02 -0800

    add price to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -18,4 +18,5 @@ class Product < ActiveRecord::Base
 
     validates_uniqueness_of :title, :flickr_tag
     validates_format_of :flickr_tag, :with => /\A[A-Za-z0-9_\-]+\z/
+    validates_format_of :price, :with => /\d{0,10}\.\d{2}/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101216072615) do
+ActiveRecord::Schema.define(:version => 20110115215439) do
 
   create_table "companies", :force => true do |t|
     t.string   "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20101216072615) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "status"
+    t.string   "price"
   end
 
 end
