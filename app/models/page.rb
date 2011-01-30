d42ff5ec211d64ce27d42fdb912b0d62f646e516
cs <cs@enure.net>
2011-01-30 14:34:55 -0800

    show if page or product is private everywhere

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,4 +3,8 @@ class Page < ActiveRecord::Base
     validates_uniqueness_of :title, :path
     validates_format_of :path, :with =>  /^[a-zA-Z_-]+$/, :message => "Must be only alpha characters, or hyphens or underscores."
 
+    def public?
+      return self.status == "Public"
+    end
+
 end
