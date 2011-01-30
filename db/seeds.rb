0f86669a2c3a54c71b4e1c5e40677734e7360def
cs <cs@enure.net>
2011-01-29 17:08:54 -0800

    add some more colors to the seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,21 @@ Color.create(
     :hex => "de41a4"
 )
 
+Color.create(
+    :title => "Black",
+    :hex => "000"
+)
+
+Color.create(
+    :title => "White",
+    :hex => "fff"
+)
+
+Color.create(
+    :title => "Gray",
+    :hex => "aaa"
+)
+
 Page.create(
     :title => "Test Page",
     :body => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
