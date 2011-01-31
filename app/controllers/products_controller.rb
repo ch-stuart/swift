7f5cabbaafe1fef0802d13dfca5640d16d8c54e4
cs <cs@enure.net>
2011-01-30 23:00:12 -0800

    simplify this controller, take out all of the xml nonsense

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,23 +3,12 @@ class ProductsController < ApplicationController
   caches_page :index, :show
   cache_sweeper :product_sweeper
 
-  # GET /products
-  # GET /products.xml
   def index
     @products = Product.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @products }
-    end
   end
 
-  # GET /products/1
-  # GET /products/1.xml
   def show
     @product = Product.find(params[:id])
-
-    # skip these because it's butt ass slow in dev
     # @photos = Product.get_photos_for_tag @product
 
     unless logged_in?
@@ -29,74 +18,47 @@ class ProductsController < ApplicationController
         return render :text => "404" 
       end
     end
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @product }
-    end
   end
 
-  # GET /products/new
-  # GET /products/new.xml
   def new
     @product = Product.new
     1.times do
       part = @product.parts.build
       # 2.times { part.colors.build }
     end
-    
-    
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @product }
-    end
   end
 
-  # GET /products/1/edit
   def edit
     @product = Product.find(params[:id])
   end
 
-  # POST /products
-  # POST /products.xml
   def create
     @product = Product.new(params[:product])
     respond_to do |format|
       if @product.save
-        format.html { redirect_to(@product, :notice => 'Product was successfully created.') }
-        format.xml  { render :xml => @product, :status => :created, :location => @product }
+        redirect_to(@product, :notice => 'Product was successfully created.')
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @product.errors, :status => :unprocessable_entity }
+        render :action => "new"
       end
     end
   end
 
-  # PUT /products/1
-  # PUT /products/1.xml
   def update
     @product = Product.find(params[:id])
 
     respond_to do |format|
       if @product.update_attributes(params[:product])
-        format.html { redirect_to(@product, :notice => 'Product was successfully updated.') }
-        format.xml  { head :ok }
+        redirect_to(@product, :notice => 'Product was successfully updated.')
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @product.errors, :status => :unprocessable_entity }
+        render :action => "edit"
       end
     end
   end
 
-  # DELETE /products/1
-  # DELETE /products/1.xml
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(products_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(products_url)
   end
 end
