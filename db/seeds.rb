262fbfe79bb3368e546055bef33bba67ed63bd43
cs <cs@enure.net>
2011-02-17 21:58:48 -0800

    make Our Story there by default

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,22 @@ Company.create(
     :address => "1422 20th AVE Seattle, WA 98122"
 )
 
+Page.create(
+    :title => "Our Story",
+    :body => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    :path => "our_story",
+    :status => "Public"
+)
+
+Product.create(
+    :title => "Test Product",
+    :description => "Test Product Description",
+    :flickr_tag => "bouncy_lions",
+    :specs => "Test Product Specs",
+    :status => "Public",
+    :price => "299.99"
+)
+
 Color.create(
     :title => "Hot Pink",
     :hex => "#de41a4"
@@ -31,20 +47,4 @@ Color.create(
 Color.create(
     :title => "Gray",
     :hex => "#aaa"
-)
-
-Page.create(
-    :title => "Test Page",
-    :body => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    :path => "test",
-    :status => "Public"
-)
-
-Product.create(
-    :title => "Test Product",
-    :description => "Test Product Description",
-    :flickr_tag => "bouncy_lions",
-    :specs => "Test Product Specs",
-    :status => "Public",
-    :price => "299.99"
 )
