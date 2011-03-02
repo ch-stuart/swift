e242099bc1b3c9e614c879d51e5e1d6550dc94b1
cs <cs@enure.net>
2011-03-01 21:56:28 -0800

    order cleanup

---
+++ public/javascripts/product_order.js
@@ -0,0 +1,55 @@
+;(function($){
+    // Behavior for order form
+    function handlers() {
+        $('input.has_price').change(function() {
+            if ($(this).attr("checked")) {
+                increment_count();
+                
+                var html = ich.product_tmpl({
+                    'hash':     $(this).data('hash'),
+                    'count':    $('#order_form').data('count'),
+                    'product':  $(this).data('product'),
+                    'price':    $(this).data('price'),
+                    'qty':      '1'
+                });
+                $('#parts').append(html);
+            } else {
+                $('#part_' + $(this).data('hash')).remove();
+            }
+        });
+        
+        $('select.has_price').change(function() {
+            var select_hash = $(this).data('hash');
+            var idx         = this.selectedIndex;
+            var options     = $(this).find('option');
+            var option      = options.get(idx);
+            var price       = $(option).data('price');
+            
+            $('#part_' + select_hash).remove();
+            
+            if (price != "") {
+                increment_count();
+                var html = ich.product_tmpl({
+                    'hash':     select_hash,
+                    'count':    $('#order_form').data('count'),
+                    'product':  $(option).data('product'),
+                    'price':    $(option).data('price'),
+                    'qty':      '1'
+                });
+                $('#parts').append(html);
+            }
+        });
+    }
+    
+    function increment_count() {
+        // get the product current count, increment it.
+        var count = parseInt($('#order_form').data('count'));
+        $('#order_form').data('count', count + 1);
+    }
+    
+    $.productOrder = function() {
+        handlers();
+        increment_count();
+    };
+
+})(jQuery);
