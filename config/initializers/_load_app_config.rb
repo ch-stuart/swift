17357a1cc3396504769c6c3fa7a5ebe081cde7ce
cs <cs@enure.net>
2011-03-02 23:37:19 -0800

    rename app config switch flickr to connect to the swift acct

--- config/initializers/_load_app_config.rb
+++ config/initializers/_load_app_config.rb
@@ -1,3 +1,3 @@
-env_config = YAML.load_file("#{Rails.root}/config/app.yml")[Rails.env]
-app_config = YAML.load_file("#{Rails.root}/config/app.yml")['application']
+env_config = YAML.load_file("#{Rails.root}/config/app_config.yml")[Rails.env]
+app_config = YAML.load_file("#{Rails.root}/config/app_config.yml")['application']
 APP_CONFIG = env_config.merge(app_config)
