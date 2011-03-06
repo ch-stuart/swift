e2c014757856ed1a7d69531c5be92aae4e022305
cs <cs@enure.net>
2011-03-05 21:03:56 -0800

    illustration field in form illustration image in view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @photos = Product.get_photos_for_tag @product
     @company = Company.first
+    @illustration = Product.get_photo_by_id "5486540020"
   end
 
   def order
