2bd3a57b3a69914951b10b5485ef73eb83368f89
cs <cs@enure.net>
2011-03-13 11:35:39 -0700

    add a bunch of crap even thought it's not used

---
+++ app/controllers/sizes_controller.rb
@@ -0,0 +1,74 @@
+class SizesController < ApplicationController
+
+  before_filter :authenticate
+  cache_sweeper SizeSweeper
+
+  def index
+    @sizes = Size.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @sizes }
+    end
+  end
+
+  def show
+    @size = Size.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @size }
+    end
+  end
+
+  def new
+    @size = Size.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @size }
+    end
+  end
+
+  def edit
+    @size = Size.find(params[:id])
+  end
+
+  def create
+    @size = Size.new(params[:size])
+
+    respond_to do |format|
+      if @size.save
+        format.html { redirect_to(@size, :notice => 'Size was successfully created.') }
+        format.xml  { render :xml => @size, :status => :created, :location => @size }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @size.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @size = Size.find(params[:id])
+
+    respond_to do |format|
+      if @size.update_attributes(params[:size])
+        format.html { redirect_to(@size, :notice => 'Size was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @size.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @size = Size.find(params[:id])
+    @size.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(sizes_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
---
+++ app/controllers/testimonials_controller.rb
@@ -0,0 +1,74 @@
+class TestimonialsController < ApplicationController
+
+  before_filter :authenticate
+  cache_sweeper TestimonialSweeper
+
+  def index
+    @testimonials = Testimonial.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @testimonials }
+    end
+  end
+
+  def show
+    @testimonial = Testimonial.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @testimonial }
+    end
+  end
+
+  def new
+    @testimonial = Testimonial.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @testimonial }
+    end
+  end
+
+  def edit
+    @testimonial = Testimonial.find(params[:id])
+  end
+
+  def create
+    @testimonial = Testimonial.new(params[:testimonial])
+
+    respond_to do |format|
+      if @testimonial.save
+        format.html { redirect_to(@testimonial, :notice => 'Testimonial was successfully created.') }
+        format.xml  { render :xml => @testimonial, :status => :created, :location => @testimonial }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @testimonial.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @testimonial = Testimonial.find(params[:id])
+
+    respond_to do |format|
+      if @testimonial.update_attributes(params[:testimonial])
+        format.html { redirect_to(@testimonial, :notice => 'Testimonial was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @testimonial.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @testimonial = Testimonial.find(params[:id])
+    @testimonial.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(testimonials_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
