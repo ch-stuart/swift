2bc84074ea4c89e3d0bee15ed677d558ec75603b
Charles Stuart <cs@enure.net>
2012-07-25 22:31:48 -0600

    better ruby

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,11 +7,11 @@ class Page < ActiveRecord::Base
   validates_format_of :path, :with =>  /^[a-zA-Z_-]+$/, :message => "Must be only alpha characters, or hyphens or underscores."
 
   def public?
-    return self.status == "Public"
+    self.status == "Public"
   end
 
   def featured?
-    return self.status == "Featured"
+    self.status == "Featured"
   end
 
 end
