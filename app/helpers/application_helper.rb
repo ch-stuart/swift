1e91ee161e15d07a5212190103310bc578f45624
Charles Stuart <cs@enure.net>
2012-12-23 17:41:01 -0700

    first progress on new layout and navigation model for home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,18 @@
 module ApplicationHelper
 
-  def md text
-    RDiscount.new(text).to_html
-  end
+    def md text
+        RDiscount.new(text).to_html
+    end
 
-  def digest str
-    Digest::MD5.hexdigest str
-  end
+    def digest str
+        Digest::MD5.hexdigest str
+    end
+
+    def set_column_classes length
+        num = "four"
+        num = "three" if length > 4
+
+        "#{num} columns"
+    end
 
 end
