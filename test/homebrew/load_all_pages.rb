3d1636a6df2769bb8a01a9793a4010c1983341f7
Charles Stuart <cs@enure.net>
2012-12-26 14:40:27 -0700

    add test

---
+++ test/homebrew/load_all_pages.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+(1..20).each do |num|
+  system "open http://builtbyswift.com/products/#{num}"
+  sleep(1)
+  system "open http://builtbyswift.com/products/#{num}/order"
+  sleep(1)
+end
+  
