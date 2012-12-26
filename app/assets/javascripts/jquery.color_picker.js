abf26b0a69a93cc21dc765ef513b40777da44907
Charles Stuart <cs@enure.net>
2012-12-26 15:17:34 -0700

    new color picker widget ties into existing form, with the exception that the priced fabrics do not get tallied up at Mals

--- app/assets/javascripts/jquery.color_picker.js
+++ app/assets/javascripts/jquery.color_picker.js
@@ -2,21 +2,24 @@ jQuery.fn.color_picker = function() {
     return this.each(function() {
 
         var $wrapper = $(this)
+        var $select = $($wrapper.data('for'))
         var $swatches = $wrapper.find('.color-picker--swatch')
         var $button = $wrapper.find('.color-picker--change')
-        
+
         $swatches.hide()
         $swatches.first().show()
-        
+
         $button.on('click', function() {
             $button.hide()
             $swatches.show()
         })
-        
+
         $swatches.on('click', function() {
             $button.show()
             $swatches.hide()
             $(this).show()
+
+            $select.get(0).selectedIndex = $(this).index() + 1
         })
 
     })
