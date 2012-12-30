ab6486902765edd724f985934d4292b1bd077052
Charles Stuart <cs@enure.net>
2012-12-30 15:28:19 -0700

    add "summary" for our story/pages

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,12 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120726040027) do
+ActiveRecord::Schema.define(:version => 20121230220908) do
 
   create_table "categories", :force => true do |t|
-    t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "title"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "colors", :force => true do |t|
@@ -53,15 +54,16 @@ ActiveRecord::Schema.define(:version => 20120726040027) do
   end
 
   create_table "pages", :force => true do |t|
-    t.string    "title"
-    t.text      "body"
-    t.string    "path"
-    t.string    "status"
-    t.string    "video_html"
-    t.string    "flickr_tag"
-    t.string    "featured"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "title"
+    t.text     "body"
+    t.string   "path"
+    t.string   "status"
+    t.string   "video_html"
+    t.string   "flickr_tag"
+    t.string   "featured"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "summary"
   end
 
   create_table "parts", :force => true do |t|
