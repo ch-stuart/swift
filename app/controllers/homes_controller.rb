b11920f25510d09bb56aba003bb0f9dbcad2c5f0
Charles Stuart <cs@enure.net>
2013-01-02 21:52:14 -0700

    use verdana for the color swatch so it's more readable

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,28 +1,28 @@
 class HomesController < ApplicationController
 
-    require 'open-uri'
+    require "open-uri"
 
     before_filter :authenticate, :except => [ :index, :store ]
     # caches_page :index, :store
     caches_action :index, :store
 
     def index
-        @pages = Page.find_all_by_status('Public')
-        @featured_page = Page.find_by_featured('Featured')
-        @products = Product.where(:status => 'Public', :kind => 'Product')
+        @pages = Page.find_all_by_status("Public")
+        @featured_page = Page.find_by_featured("Featured")
+        @products = Product.where(:status => "Public", :kind => "Product")
         @categories = Category.all
-        @accessories = Product.where(:status => 'Public', :kind => 'Accessory')
+        @accessories = Product.where(:status => "Public", :kind => "Accessory")
         @company = Company.first
 
         @blog = get_latest_blog_post
 
-        @photos = Home.get_photos
+        @photos = Home.get_photos_by_set "72157631915465787"
     end
 
     def store
-        @products = Product.where(:status => 'Public', :kind => 'Product')
-        @accessories = Product.where(:status => 'Public', :kind => 'Accessory')
-        @stock = Product.where(:status => 'Public', :kind => 'Stock')
+        @products = Product.where(:status => "Public", :kind => "Product")
+        @accessories = Product.where(:status => "Public", :kind => "Accessory")
+        @stock = Product.where(:status => "Public", :kind => "Stock")
         @company = Company.first
     end
 
@@ -34,11 +34,11 @@ class HomesController < ApplicationController
         xml = open("http://cycleswift.wordpress.com/feed/")
         doc = Nokogiri::XML(xml)
 
-        @blog['title'] = doc.at_css("rss channel item title").text
-        @blog['link'] = doc.at_css("rss channel item link").text
-        @blog['description'] = doc.at_css("rss channel item description").text
+        @blog["title"] = doc.at_css("rss channel item title").text
+        @blog["link"] = doc.at_css("rss channel item link").text
+        @blog["description"] = doc.at_css("rss channel item description").text
         img = doc.css("media|content")[1]
-        @blog['img'] = img['url']
+        @blog["img"] = img["url"]
 
         @blog
     end
