ca6626b54bcff4b407e65b9d0dd74ca34f74018e
Charles Stuart <cs@enure.net>
2013-01-27 13:53:08 -0700

    animating images 1x1

--- app/assets/javascripts/jquery.hero.js
+++ app/assets/javascripts/jquery.hero.js
@@ -1,38 +1,95 @@
-// <div id="hero" class="sixteen columns">
-//     <div id="hero-scroll">
-//         <% @photos.each do |photo| %>
-//             <img src=<%= photo[:url] %>>
-//         <% end %>
-//     </div>
-// </div>
+/*
+ * An animation step manager.
+ */
+
+ /*
+     ## How to Use
+
+     // Create your runner
+     var animRunner = new Runner();
+
+     // Add animation step 1
+     animRunner.add(function() {
+         // You animation code...
+     }, 100)
+
+     // Add animation step 2, this will run
+     // 500ms after the first step completes
+     animRunner.add(function() {
+         // You animation code...
+     }, 500)
+
+     // And this will run 1s after the 2nd
+     // step completes
+     animRunner.add(function() {
+         // You animation code...
+     }, 1000)
+
+     // Run the steps
+     animRunner.run();
+ */
+
+var Runner = function(ms) {
+    this.ms = ms;
+    this.steps = [];
+    this.add = function(func, ms) {
+        if (!ms) { ms = this.ms; }
+        this.steps.push({ 'func': func, 'ms': ms });
+    };
+    this.run = function() {
+        var ms = 0;
+        this.steps.forEach(function(step) {
+            ms += step.ms;
+            setTimeout(function() {
+                step.func();
+            }, ms);
+        });
+    };
+};
 
 jQuery.fn.hero = function(params) {
 
-	var width = 0;
-	var scrollDist;
-	
-    return this.each(function() {
-        var $heroWrapper = $(this);
-        
-		$heroWrapper
-			.find('#hero-scroll')
-			.find('img')
-			.each(function() {
-				width += $(this).width()
-			});
-			
-		$heroWrapper
-			.find('#hero-scroll')
-			.css('width', width + 'px');
+	var totalWidth = 0;
+	var widths = [];
+	var imgCount = 0;
+	var imgLoadCount = 0;
+	var r = new Runner(3000);
 
-		scrollDist = width - $heroWrapper.width()
+    return this.each(function() {
+        var $hero = $(this);
+        var $heroScroller = $hero.find('#hero-scroll');
+		var $imgs = $heroScroller.find('img');
 		
-		$('#hero-scroll').animate({
-		    'margin-left': -(scrollDist)+'px'
-		},
-		{
-		    'duration': 20 * 1000
-		})
+		imgCount = $imgs.size();
 		
+		function init() {
+			$heroScroller
+				.find('img')
+				.each(function() {
+					var imgWidth = $(this).width()
+					totalWidth += imgWidth
+					widths.push(imgWidth)
+				});
+			
+			$heroScroller
+				.css('width', totalWidth + 'px');
+
+			widths.forEach(function(dist, index) {
+				r.add(function() {
+					var currentScrollDist = Math.abs(parseInt($heroScroller.css('margin-left'), 10));
+					var animDist = currentScrollDist + dist;
+				
+					console.log(animDist);
+				
+					if (animDist < (totalWidth - $hero.width()) ) {
+						$heroScroller.animate({
+							'margin-left': -(animDist) + 'px'
+						})
+					}
+				})
+			})
+			r.run();
+		}
+		$(window).load(init);
     });
 };
