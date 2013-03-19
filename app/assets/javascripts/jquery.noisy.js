71fefc780d41262776102f423b986464beb349d4
Charles Stuart <cs@enure.net>
2013-03-18 20:50:20 -0600

    update jquery noisy

--- app/assets/javascripts/jquery.noisy.js
+++ app/assets/javascripts/jquery.noisy.js
@@ -1,39 +1,64 @@
-(function($){
+;(function($){
 	// This function adds noise to the background-image attribute of a given element
 	$.fn.noisy = function(options) {
+		options = $.extend({}, $.fn.noisy.defaults, options);
+		var uri, localStorageSupported, cachedUri = false;
+		
+		try {
+			localStorageSupported = true;
+			cachedUri = localStorage.getItem(window.JSON.stringify(options));
+		} catch(e) {
+			localStorageSupported = false;
+		}
+		
+		// Use localStorage cache if these options have been used before
+		if (cachedUri) {
+			uri = cachedUri;
+		}
+		else {
+			var canvas = document.createElement('canvas');
+			
+			// Use fallback image if canvas isn't supported
+			if (!canvas.getContext) {
+				uri = options.fallback;
+			}
+			else {
+				canvas.width = canvas.height = options.size;
+			
+				var ctx = canvas.getContext('2d'),
+				    imgData = ctx.createImageData(canvas.width, canvas.height),
+				    numPixels = Math.round( options.intensity * Math.pow(options.size, 2) ),
+				    maxAlpha = 255 * options.opacity;
+				    
+				// Add color to random pixels in the canvas
+				while (numPixels--) { // Read about the double bitwise NOT trick here: goo.gl/6DPpt
+					var x = ~~(Math.random()*canvas.width),
+					    y = ~~(Math.random()*canvas.height),
+					    index = (x + y * imgData.width) * 4;
+					
+					var colorChannel = numPixels % 255; // This will look random enough
+					imgData.data[index  ] = colorChannel;                                               // red
+					imgData.data[index+1] = options.monochrome ? colorChannel : ~~(Math.random()*255);  // green
+					imgData.data[index+2] = options.monochrome ? colorChannel : ~~(Math.random()*255);  // blue
+					imgData.data[index+3] = ~~(Math.random()*maxAlpha);                                 // alpha
+				}
+				
+				ctx.putImageData(imgData, 0, 0);
+				uri = canvas.toDataURL('image/png');
+				
+				// toDataURL doesn't return anything in Android 2.2
+				if (uri.indexOf('data:image/png') != 0) {
+					uri = options.fallback;
+				}
+			}
+			
+			if (window.JSON && localStorageSupported) {
+				localStorage.setItem(window.JSON.stringify(options), uri);
+			}
+		}
+		
 		return this.each(function() {
-	 		var canvas = document.createElement('canvas'),
-	 		    ctx = canvas.getContext("2d");
-	 		
-	 		// Use fallback image if canvas isn't supported
-	 		if (!ctx && (options.fallback !== undefined) && (options.fallback !== '')) {
-	 			$(this).css('background-image', 'url(' + options.fallback + ')');
-	 			return;
-	 		}
-	 		
-	 		options = $.extend({}, $.fn.noisy.defaults, options);
-	 		
-	 		canvas.width = canvas.height = options.size;
-	 		var imgData = ctx.createImageData(canvas.width, canvas.height);
-	 		
-	 		var rand = function(min, max) {
-	 			return Math.floor(Math.random()*(max-min)+min);
-	 		};
-	 		
-	 		// Add pixels at random positions to the canvas
-	 		for (var i = 0; i < options.intensity * Math.pow(options.size, 2); i++) {
-	 			var x = rand(0, canvas.width),
-	 			    y = rand(0, canvas.height),
-	 			    index = (x + y * imgData.width) * 4;
-	 			
-	 			var randColorChannel = rand(0, 255);
-	 			imgData.data[index  ] = randColorChannel;                                           // red
-	 			imgData.data[index+1] = options.monochrome ? randColorChannel : rand(0, 255);       // green
-	 			imgData.data[index+2] = options.monochrome ? randColorChannel : rand(0, 255);       // blue
-	 			imgData.data[index+3] = rand(0, 255 * options.opacity);                             // alpha
-	 		}
-	 		ctx.putImageData(imgData, 0, 0);
-	 		$(this).css('background-image', 'url(' + canvas.toDataURL('image/png') + ')');
+	 		$(this).css('background-image', "url('" + uri + "')," + $(this).css('background-image'));
 		});
 	};
 	$.fn.noisy.defaults = {
