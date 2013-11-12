a002e32177ef1a119fb739ba6c1efc33e4849fe5
Charles Stuart <charles.stuart@bodybuilding.com>
2013-11-11 20:24:58 -0700

    Fix issue where Flickr ID validation became incorrect b/c Flickr IDs changed

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,7 @@ class Product < ActiveRecord::Base
   accepts_nested_attributes_for :testimonials, :reject_if => lambda { |a| a[:body].blank? }, :allow_destroy => true
   accepts_nested_attributes_for :sizes, :reject_if => lambda { |a| a[:title].blank? }, :allow_destroy => true
 
-  FLICKR_ID_MATCH = /^\d{10}$/
+  FLICKR_ID_MATCH = /^\d{10,11}$/
   FLICKR_SET_MATCH = /^\d{17}$/
   
   KINDS = ["Product", "Accessory", "Stock"]
@@ -30,8 +30,8 @@ class Product < ActiveRecord::Base
   validates_uniqueness_of :title, :short_title
   validates_format_of :flickr_tag, :with => /^\A[A-Za-z0-9_\-]+\z$/, :if => :flickr_tag?
   validates_format_of :price, :with => /^\d{0,10}\.\d{2}$/, :message => "must be include dollars and cents, ex: 122.22"
-  validates_format_of :flickr_photo, :with => FLICKR_ID_MATCH, :message => "is 10 digits long, all numbers.", :if => :flickr_photo?
-  validates_format_of :flickr_set, :with => FLICKR_SET_MATCH, :message => "is 10 digits long, all numbers.", :if => :flickr_set?
+  validates_format_of :flickr_photo, :with => FLICKR_ID_MATCH, :message => "is 10 or 11 digits long, all numbers.", :if => :flickr_photo?
+  validates_format_of :flickr_set, :with => FLICKR_SET_MATCH, :message => "is 17 digits long, all numbers.", :if => :flickr_set?
 
   def public?
     self.status == "Public"
