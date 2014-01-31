2f98a5efac1423335b953c2b99d956f857c33c35
Charles Stuart <charles.stuart@bodybuilding.com>
2014-01-30 22:58:49 -0700

    Remove no longer valid test cases

--- test/casperjs/purchase_product.js
+++ test/casperjs/purchase_product.js
@@ -21,24 +21,6 @@ casper.test.begin('Can purchase product', function suite(test) {
         this.test.assert(isOrderPage, 'got to order page')
     })
 
-    casper.then(function() {
-        this.click('#submit_order')
-    })
-
-    casper.then(function() {
-        this.test.assertTitle('Cart', 'Got to Mals')
-        this.test.assertTextExists('Prod1 Title', 'product is in cart')
-    })
-
-    casper.then(function() {
-        this.click('a:first-child')
-    })
-
-    casper.then(function() {
-        var title = 'Swift Industries | handmade bicycle panniers and accessories';
-        this.test.assertTitle(title, 'Got back to builtbyswift.com')
-    })
-
     casper.run(function() {
         test.done()
     })
