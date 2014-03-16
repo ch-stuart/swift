d84990a0c21101874ec7edd4ea02776d727a6cc2
Charles Stuart <charles.stuart@bodybuilding.com>
2014-03-15 21:53:49 -0600

    Set time zone to PST

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -5,7 +5,7 @@ class SalesController < ApplicationController
   # GET /sales
   # GET /sales.json
   def index
-    @sales = Sale.all
+    @sales = Sale.all.reverse
 
     render :layout => "hub"
     #
