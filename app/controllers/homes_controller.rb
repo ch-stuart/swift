69a1ec591b8ded7742e80cf3aea70f4557b6de8a
Charles Stuart <charles.stuart@bodybuilding.com>
2014-04-06 09:44:42 -0600

    Don't allow exception when retrieving latest blog post to stop home page from loading.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -43,14 +43,18 @@ class HomesController < ApplicationController
     def get_latest_blog_post
         @blog = {}
 
-        xml = open("http://cycleswift.wordpress.com/feed/")
-        doc = Nokogiri::XML(xml)
+        begin
+          xml = open("http://cycleswift.wordpress.com/feed/")
+          doc = Nokogiri::XML(xml)
 
-        @blog["title"] = doc.at_css("rss channel item title").text
-        @blog["link"] = doc.at_css("rss channel item link").text
-        @blog["description"] = doc.at_css("rss channel item description").text
-        img = doc.css("media|content")[1]
-        @blog["img"] = img["url"]
+          @blog["title"] = doc.at_css("rss channel item title").text
+          @blog["link"] = doc.at_css("rss channel item link").text
+          @blog["description"] = doc.at_css("rss channel item description").text
+          img = doc.css("media|content")[1]
+          @blog["img"] = img["url"]
+        rescue Exception => e
+          logger.error e
+        end
 
         @blog
     end
