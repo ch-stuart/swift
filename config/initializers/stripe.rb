1f59cfb21c0f210c57c44ac786e4de22f2af2ef4
Charles Stuart <cs@enure.net>
2014-04-27 11:06:08 -0600

    Fix logging

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -6,7 +6,7 @@ if Rails.env == 'production'
   }
   Stripe.api_key = Rails.configuration.stripe[:secret_key]
 else
-  Rails.logger.info 'Using Stripe prod key'
+  Rails.logger.info 'Using Stripe test key'
   Rails.configuration.stripe = {
     publishable_key: ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
     secret_key:      ENV['STRIPE_TEST_SECRET_KEY'],
