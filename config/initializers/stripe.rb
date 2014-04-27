9684e8e3ab6b50dda9dd16756db79f6643cc738c
Charles Stuart <cs@enure.net>
2014-04-27 10:50:10 -0600

    Update stripe config to use Martina's account

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,15 @@
-Rails.configuration.stripe = {
-  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
-  secret_key:      ENV['STRIPE_SECRET_KEY'],
-}
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+if Rails.env == 'production'
+  Rails.logger.info 'Using Stripe prod key'
+  Rails.configuration.stripe = {
+    publishable_key: ENV['STRIPE_PROD_PUBLISHABLE_KEY'],
+    secret_key:      ENV['STRIPE_PROD_SECRET_KEY'],
+  }
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]
+else
+  Rails.logger.info 'Using Stripe prod key'
+  Rails.configuration.stripe = {
+    publishable_key: ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+    secret_key:      ENV['STRIPE_TEST_SECRET_KEY'],
+  }
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]
+end
