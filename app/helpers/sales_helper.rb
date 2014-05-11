1c0d8bc3de8edfcfe0f88e36b943e20c12144647
Charles Stuart <cs@enure.net>
2014-05-11 17:50:58 -0600

    Bug fixes

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -4,4 +4,14 @@ module SalesHelper
         num.to_f / 100
     end
 
+    def last_name sale
+        return "n/a" if sale.contact.nil?
+
+        if sale.contact.include? " "
+            sale.contact.split(" ").last
+        else
+            sale.contact
+        end
+    end
+
 end
