a9e52bd84074e8de00dfc2bc49a951a1a3411ce7
Charles Stuart <cs@enure.net>
2014-05-31 16:08:46 -0600

    Include UA and location.href

--- app/assets/javascripts/global_exception_service.js
+++ app/assets/javascripts/global_exception_service.js
@@ -1,8 +1,13 @@
 /*global window jQuery */
 
 window.onerror = function(errorMsg, url, lineNumber) {
-    var string = JSON.stringify({ 'msg': errorMsg, 'url': url, 'lineNumber': lineNumber });
-    var data = { msg: string };
+    var string = JSON.stringify({
+        'msg': errorMsg,
+        'url': url,
+        'lineNumber': lineNumber,
+        'userAgent': window.navigator.userAgent,
+        'href': window.location.href
+    });
 
-    jQuery.post("/exceptions/report", data);
+    jQuery.post("/exceptions/report", { msg: string });
 };
