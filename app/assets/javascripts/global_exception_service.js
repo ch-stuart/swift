2cf3332ad82621d964afd145b6e4c75e88a7c961
Charles Stuart <cs@enure.net>
2014-05-31 17:20:44 -0600

    Don't bother reporting if there isn't a errorMsg

--- app/assets/javascripts/global_exception_service.js
+++ app/assets/javascripts/global_exception_service.js
@@ -1,6 +1,9 @@
 /*global window jQuery */
 
 window.onerror = function(errorMsg, url, lineNumber) {
+    // Don't bother if there isn't a errorMsg
+    if (!errorMsg) return;
+
     var string = JSON.stringify({
         'msg': errorMsg,
         'url': url,
