a718722f967c22218c1fee4863b8e80242ce9725
Charles Stuart <cs@enure.net>
2014-05-31 17:24:30 -0600

    jeepers

--- app/assets/javascripts/global_exception_service.js
+++ app/assets/javascripts/global_exception_service.js
@@ -2,7 +2,7 @@
 
 window.onerror = function(errorMsg, url, lineNumber) {
     // Don't bother if there isn't a errorMsg
-    if (!errorMsg) return;
+    if (!errorMsg || errorMsg === 'Script error.') return;
 
     var string = JSON.stringify({
         'msg': errorMsg,
