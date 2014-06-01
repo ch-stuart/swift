3c83590f917535f5d252396e12ea3e3d04390042
Charles Stuart <cs@enure.net>
2014-05-31 20:52:36 -0600

    schema updates for package_type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140423043010) do
+ActiveRecord::Schema.define(:version => 20140531203055) do
 
   create_table "categories", :force => true do |t|
     t.string   "title"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(:version => 20140423043010) do
     t.integer  "height"
     t.integer  "length"
     t.float    "weight"
+    t.string   "package_type",           :default => "CUSTOM"
   end
 
   create_table "sales", :force => true do |t|
