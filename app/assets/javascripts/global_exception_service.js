aa2e8be741051ae4e6e59678f9c4d3ce8a526d7f
Charles Stuart <cs@enure.net>
2014-06-03 19:31:35 -0600

    add a block list to global exception service
    prettify output
    
    don't use JSON

--- app/assets/javascripts/global_exception_service.js
+++ app/assets/javascripts/global_exception_service.js
@@ -1,16 +1,45 @@
 /*global window jQuery */
 
-window.onerror = function(errorMsg, url, lineNumber) {
-    // Don't bother if there isn't a errorMsg
-    if (!errorMsg || errorMsg === 'Script error.') return;
+;(function(window, jQuery) {
+    var msgBlockList = [
+        'Script error.',
+        'ReferenceError: Can\'t find variable: cafe',
+        'ReferenceError: Can\'t find variable: dataKeys'
+    ];
 
-    var string = JSON.stringify({
-        'msg': errorMsg,
-        'url': url,
-        'lineNumber': lineNumber,
-        'userAgent': window.navigator.userAgent,
-        'href': window.location.href
-    });
+    var scriptUrlBlockList = [
+        'http://assets.pinterest.com/js/pinit.js'
+    ];
 
-    jQuery.post("/exceptions/report", { msg: string });
-};
+    window.onerror = function(msg, url, lineNumber) {
+        var string = '';
+
+        // Don't bother if there isn't a msg
+        if (!msg) return;
+
+        for (var m = msgBlockList.length - 1; m >= 0; m--) {
+            if (msg === msgBlockList[m]) return;
+        }
+
+        for (var s = scriptUrlBlockList.length - 1; s >= 0; s--) {
+            if (url === scriptUrlBlockList[s]) return;
+        }
+
+        string += '== Error\n';
+        string += msg + '\n\n';
+
+        string += '== Script URL\n';
+        string += url + '\n\n';
+
+        string += '== lineNumber\n';
+        string += lineNumber + '\n\n';
+
+        string += '== userAgent\n';
+        string += window.navigator.userAgent + '\n\n';
+
+        string += '== URL\n';
+        string += window.location.href + '\n\n';
+
+        jQuery.post("/exceptions/report", { msg: string });
+    };
+})(window, jQuery);
