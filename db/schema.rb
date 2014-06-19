5c531e8ec16537ac057ec9fe58c8f74741f4cc93
Charles Stuart <cs@enure.net>
2014-06-18 21:51:11 -0600

    add wholesale attr to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140619025705) do
+ActiveRecord::Schema.define(:version => 20140619034918) do
 
   create_table "categories", :force => true do |t|
     t.string   "title"
@@ -196,6 +196,7 @@ ActiveRecord::Schema.define(:version => 20140619025705) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "admin",                  :default => false
+    t.boolean  "wholesale",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
