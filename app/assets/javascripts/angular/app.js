1b8893721055ec6704a35b921ce6c19827c5a8f6
Charles Stuart <cs@enure.net>
2014-06-25 08:51:45 -0600

    Fix issue where making an ajax post logged out the user

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,1 +1,8 @@
+/*global angular $ */
 var SwiftApp = angular.module('SwiftApp', []);
+
+SwiftApp.config([
+  "$httpProvider", function($httpProvider) {
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+  }
+]);
