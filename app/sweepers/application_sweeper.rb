3cc147b7e749dfbe257bddc6d16691094b3efb66
Charles Stuart <cs@enure.net>
2014-06-26 22:53:24 -0600

    Fix up cache sweeping.

--- app/sweepers/application_sweeper.rb
+++ app/sweepers/application_sweeper.rb
@@ -18,34 +18,52 @@ class ApplicationSweeper < ActionController::Caching::Sweeper
     end
 
     def expire_cache
-        expire_action :controller => 'homes', :action => 'index', :user_type => 'WS'
-        expire_action :controller => 'homes', :action => 'index', :user_type => 'STANDARD'
-        expire_action :controller => 'homes', :action => 'store', :user_type => 'WS'
-        expire_action :controller => 'homes', :action => 'store', :user_type => 'STANDARD'
+        expire_action :controller => 'homes', :action => 'index', :user_type => 'WHOLESALE'
+        expire_action :controller => 'homes', :action => 'index', :user_type => 'ADMIN'
+        expire_action :controller => 'homes', :action => 'index', :user_type => 'USER_SIGNED_IN'
+        expire_action :controller => 'homes', :action => 'index', :user_type => 'USER_NOT_SIGNED_IN'
+        expire_action :controller => 'homes', :action => 'store', :user_type => 'WHOLESALE'
+        expire_action :controller => 'homes', :action => 'store', :user_type => 'ADMIN'
+        expire_action :controller => 'homes', :action => 'store', :user_type => 'USER_SIGNED_IN'
+        expire_action :controller => 'homes', :action => 'store', :user_type => 'USER_NOT_SIGNED_IN'
 
         Product.all.each do |product|
-            expire_action :controller => 'products', :action => 'show', :id => product.id, :user_type => 'WS'
-            expire_action :controller => 'products', :action => 'show', :id => product.id, :user_type => 'STANDARD'
-            expire_action :controller => 'products', :action => 'order', :id => product.id, :user_type => 'WS'
-            expire_action :controller => 'products', :action => 'order', :id => product.id, :user_type => 'STANDARD'
-            expire_action :controller => 'products', :action => 'show', :id => product.id, :user_type => 'WS', :format => 'json'
-            expire_action :controller => 'products', :action => 'show', :id => product.id, :user_type => 'STANDARD', :format => 'json'
+            expire_action :controller => 'products', :action => 'show', :id => product.id, :user_type => 'WHOLESALE'
+            expire_action :controller => 'products', :action => 'show', :id => product.id, :user_type => 'ADMIN'
+            expire_action :controller => 'products', :action => 'show', :id => product.id, :user_type => 'USER_SIGNED_IN'
+            expire_action :controller => 'products', :action => 'show', :id => product.id, :user_type => 'USER_NOT_SIGNED_IN'
+            expire_action :controller => 'products', :action => 'order', :id => product.id, :user_type => 'WHOLESALE'
+            expire_action :controller => 'products', :action => 'order', :id => product.id, :user_type => 'ADMIN'
+            expire_action :controller => 'products', :action => 'order', :id => product.id, :user_type => 'USER_SIGNED_IN'
+            expire_action :controller => 'products', :action => 'order', :id => product.id, :user_type => 'USER_NOT_SIGNED_IN'
+            expire_action :controller => 'products', :action => 'show', :id => product.id, :user_type => 'WHOLESALE', :format => 'json'
+            expire_action :controller => 'products', :action => 'show', :id => product.id, :user_type => 'ADMIN', :format => 'json'
+            expire_action :controller => 'products', :action => 'show', :id => product.id, :user_type => 'USER_SIGNED_IN', :format => 'json'
+            expire_action :controller => 'products', :action => 'show', :id => product.id, :user_type => 'USER_NOT_SIGNED_IN', :format => 'json'
         end
 
         Page.all.each do |page|
-            expire_action :controller => 'pages', :action => 'show', :id => page.id, :user_type => 'WS'
-            expire_action :controller => 'pages', :action => 'show', :id => page.id, :user_type => 'STANDARD'
+            expire_action :controller => 'pages', :action => 'show', :id => page.id, :user_type => 'WHOLESALE'
+            expire_action :controller => 'pages', :action => 'show', :id => page.id, :user_type => 'ADMIN'
+            expire_action :controller => 'pages', :action => 'show', :id => page.id, :user_type => 'USER_SIGNED_IN'
+            expire_action :controller => 'pages', :action => 'show', :id => page.id, :user_type => 'USER_NOT_SIGNED_IN'
         end
 
-        expire_action :controller => 'sales', :action => 'cart', :user_type => 'WS'
-        expire_action :controller => 'sales', :action => 'cart', :user_type => 'STANDARD'
-        expire_action :controller => 'sales', :action => 'checkout', :user_type => 'WS'
-        expire_action :controller => 'sales', :action => 'checkout', :user_type => 'STANDARD'
+        expire_action :controller => 'sales', :action => 'cart', :user_type => 'WHOLESALE'
+        expire_action :controller => 'sales', :action => 'cart', :user_type => 'ADMIN'
+        expire_action :controller => 'sales', :action => 'cart', :user_type => 'USER_SIGNED_IN'
+        expire_action :controller => 'sales', :action => 'cart', :user_type => 'USER_NOT_SIGNED_IN'
+        expire_action :controller => 'sales', :action => 'checkout', :user_type => 'WHOLESALE'
+        expire_action :controller => 'sales', :action => 'checkout', :user_type => 'ADMIN'
+        expire_action :controller => 'sales', :action => 'checkout', :user_type => 'USER_SIGNED_IN'
+        expire_action :controller => 'sales', :action => 'checkout', :user_type => 'USER_NOT_SIGNED_IN'
 
         # TODO figure out how to expire cache for success action
         # Sale.all.each do |sale|
-        #     expire_action :controller => 'sales', :action => 'success', :guid => sale.guid, :user_type => 'STANDARD'
-        #     expire_action :controller => 'sales', :action => 'success', :guid => sale.guid, :user_type => 'WS'
+        #     expire_action :controller => 'sales', :action => 'success', :guid => sale.guid, :user_type => 'ADMIN'
+        #     expire_action :controller => 'sales', :action => 'success', :guid => sale.guid, :user_type => 'WHOLESALE'
+        #     expire_action :controller => 'sales', :action => 'success', :guid => sale.guid, :user_type => 'USER_SIGNED_IN'
+        #     expire_action :controller => 'sales', :action => 'success', :guid => sale.guid, :user_type => 'USER_NOT_SIGNED_IN'
         # end
 
         # this is a bit of a mess
