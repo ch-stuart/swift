3f8d92195c28eae3d8a68657881aa7df2d66517e
Charles Stuart <cs@enure.net>
2014-06-29 12:34:56 -0600

    Show pricing diff on sales index

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -15,7 +15,41 @@ class Sale < ActiveRecord::Base
 
   validates_presence_of :email, :amount, :total, :stripe_id
 
+  def get_shipping_price_diff
+    # expected is what we charged the customer
+    # actual is what we paid to ship
+
+    if self.shipping_charge && self.shipments.present?
+      expected = self.shipping_charge.to_i
+
+      shipping_costs = self.shipments.map do |shipment|
+        shipment.cost.to_i
+      end
+      actual = shipping_costs.inject{|sum,x| sum + x }
+
+      if expected == actual
+        "MATCHED"
+      elsif expected > actual
+        diff = expected - actual
+        diff_percent = get_diff_percent diff, expected
+
+        return ["+", diff, diff_percent]
+      else
+        diff = actual - expected
+        diff_percent = get_diff_percent diff, expected
+
+        return ["-", diff, diff_percent]
+      end
+    else
+      ""
+    end
+  end
+
   private
+
+  def get_diff_percent diff, expected
+    ((diff.to_f / expected.to_f) * 100).to_s.split('.')[0]
+  end
 
   def populate_guid
     self.guid = SecureRandom.hex(4)
