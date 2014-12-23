580c8b2c5d1c43fce90ee15c504f578bd5a687ef
Charles Stuart <charles.stuart@gmail.com>
2014-12-23 12:34:05 -0700

    temp fix for broken homepage due to lacking hi res image

--- app/modules/flickr.rb
+++ app/modules/flickr.rb
@@ -146,12 +146,12 @@ module Flickr
       small_320 = sizes.find {|size| size.label == "Small 320" }
 
       photos.push({
-        :id => photo["id"],
-        :small_320_url => small_320.source,
-        :medium_url => medium.source,
-        :url => medium.source,
-        :medium_640_url => medium_640.source,
-        :medium_800_url => medium_800.source
+        id: photo["id"],
+        small_320_url: small_320.source,
+        # medium_url: medium.source,
+        # url: medium.source,
+        medium_640_url: medium_640.source
+        # medium_800_url: medium_800.source
       })
     end
 
