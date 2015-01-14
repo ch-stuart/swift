073ec56b130c1b0358cdbf0b9447459f111c4a6a
Charles Stuart <charles.stuart@gmail.com>
2015-01-13 22:11:30 -0700

    remove logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,6 @@ class ApplicationController < ActionController::Base
   end
 
   def verify_is_admin
-    logger.info "== CURRENT_USER is #{current_user.inspect}"
     if current_user.try(:admin?)
       return
     else
