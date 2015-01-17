e3d9b51a37bc0ce7d74a7d2e6e4da3db76a5da4d
Charles Stuart <charles.stuart@gmail.com>
2015-01-17 16:11:51 -0700

    Delete commented out code

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,7 +1,5 @@
 class Part < ActiveRecord::Base
 
-    # attr_accessible :title, :price, :product_id, :wholesale_price, :color_ids
-
     belongs_to :product
     has_and_belongs_to_many :colors
     accepts_nested_attributes_for :colors, :reject_if => lambda { |a| a[:title].blank? }, :allow_destroy => true
--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,7 +2,6 @@ class Shipment < ActiveRecord::Base
 
   belongs_to :sale
 
-  # attr_accessible       :postmaster_id, :tracking_number, :carrier, :weight, :width, :height, :length, :sale_id, :cost, :envelope
   validates_presence_of :postmaster_id, :tracking_number, :carrier, :weight, :sale_id, :cost
 
 end
