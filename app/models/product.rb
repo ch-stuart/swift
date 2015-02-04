bf426ab4922dc738c2000a19975e79da3bac35c4
Charles Stuart <charles.stuart@gmail.com>
2015-02-03 21:14:16 -0700

    add touch: true to belongs_to in order to fix caching
    also change from hashrocket to colon syntax

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,30 +2,30 @@ class Product < ActiveRecord::Base
 
   extend Flickr
 
-  has_many :parts, :dependent => :destroy
-  has_many :testimonials, :dependent => :destroy
-  has_many :sizes, :dependent => :destroy
-  belongs_to :category
+  has_many :parts, dependent: :destroy
+  has_many :testimonials, dependent: :destroy
+  has_many :sizes, dependent: :destroy
+  belongs_to :category, touch: true
 
-  accepts_nested_attributes_for :parts, :reject_if => lambda { |a| a[:title].blank? }, :allow_destroy => true
-  accepts_nested_attributes_for :testimonials, :reject_if => lambda { |a| a[:body].blank? }, :allow_destroy => true
-  accepts_nested_attributes_for :sizes, :reject_if => lambda { |a| a[:title].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :parts, reject_if: lambda { |a| a[:title].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :testimonials, reject_if: lambda { |a| a[:body].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :sizes, reject_if: lambda { |a| a[:title].blank? }, allow_destroy: true
 
   FLICKR_ID_MATCH = /\A\d{10,11}\z/
   FLICKR_SET_MATCH = /\A\d{17}\z/
 
   KINDS = ["Product", "Accessory", "Stock", "Gift Certificate", "Event"]
   attr_reader :KINDS
-  validates :kind, :inclusion => { :in => KINDS, :message => "%{value} is not a valid type" }
+  validates :kind, inclusion: { in: KINDS, message: "%{value} is not a valid type" }
 
   # PACKAGE_TYPE = ["CUSTOM", "LETTER", "PAK"]
   PACKAGE_TYPE = ["CUSTOM"]
   attr_reader :PACKAGE_TYPE
-  validates :package_type, :inclusion => { :in => PACKAGE_TYPE, :message => "%{value} is not a valid type" }
+  validates :package_type, inclusion: { in: PACKAGE_TYPE, message: "%{value} is not a valid type" }
 
   STATUSES = ["Public", "Private"]
   attr_reader :STATUSES
-  validates :status, :inclusion => { :in => STATUSES, :message => "%{value} is not a valid status" }
+  validates :status, inclusion: { in: STATUSES, message: "%{value} is not a valid status" }
 
   validates_presence_of :title, :short_title, :flickr_tag, :flickr_photo, :price, :humane_price, :wholesale_price, :wholesale_humane_price
   # Accessories and Stock cannot have categories, otherwise they show up in the
@@ -33,14 +33,14 @@ class Product < ActiveRecord::Base
   validates_presence_of :category_id, :if => :is_product?
 
   validates_uniqueness_of :title, :short_title
-  validates_format_of :flickr_tag, :with => /\A[A-Za-z0-9_\-]+\z/, :if => :flickr_tag?
-  validates_format_of :flickr_photo, :with => FLICKR_ID_MATCH, :message => "is 10 digits long, all numbers.", :if => :flickr_photo?
-  validates_format_of :flickr_set, :with => FLICKR_SET_MATCH, :message => "is 17 digits long, all numbers.", :if => :flickr_set?
+  validates_format_of :flickr_tag, with: /\A[A-Za-z0-9_\-]+\z/, if: :flickr_tag?
+  validates_format_of :flickr_photo, with: FLICKR_ID_MATCH, message: "is 10 digits long, all numbers.", if: :flickr_photo?
+  validates_format_of :flickr_set, with: FLICKR_SET_MATCH, message: "is 17 digits long, all numbers.", if: :flickr_set?
 
   PRICE_MATCH = /\A\d{0,10}\.\d{2}\z/
   PRICE_MESSAGE = "must be in the following format: 12.00"
-  validates_format_of :price, :with => PRICE_MATCH, :message => PRICE_MESSAGE, :if => :price?
-  validates_format_of :wholesale_price, :with => PRICE_MATCH, :message => PRICE_MESSAGE, :if => :wholesale_price?
+  validates_format_of :price, with: PRICE_MATCH, message: PRICE_MESSAGE, if: :price?
+  validates_format_of :wholesale_price, with: PRICE_MATCH, message: PRICE_MESSAGE, if: :wholesale_price?
 
   before_save :serialize_related_products
 
