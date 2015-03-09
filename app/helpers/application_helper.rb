71b4d3ffe3251cf9bebf2b660ed9b06f485fd44c
Charles Stuart <charles.stuart@gmail.com>
2015-03-08 18:21:45 -0600

    Switch to Flexboxgrid

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,11 @@
 module ApplicationHelper
 
     def md text
-        RDiscount.new(text).to_html
-    end
-
-    def set_column_classes length
-        num = "four"
-        num = "three" if length > 4
-
-        "#{num} columns"
+      RDiscount.new(text).to_html
     end
 
     def cents_to_dollars num
-        num.to_f / 100
+      num.to_f / 100
     end
 
     # Acts as a thin wrapper for image_tag and generates an srcset attribute for regular image tags
