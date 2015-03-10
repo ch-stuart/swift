e6f7b538d54c887f112d8d92cd8c1707ddbad3fd
Charles Stuart <charles.stuart@gmail.com>
2015-03-09 21:52:10 -0600

    whitespace

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,37 +2,38 @@ class RegistrationsController < Devise::RegistrationsController
   before_filter :update_sanitized_params, if: :devise_controller?
 
   def update_sanitized_params
+
     devise_parameter_sanitizer
       .for(:sign_up) do |u|
-
-      u.permit(
-        :is_attending_campout_in_2015,
-        :is_pending_dealer,
-        :email,
-        :password,
-        :password_confirmation,
-        :contact,
-        :city,
-        :state,
-        :zip_code
-      )
+        u.permit(
+          :is_attending_campout_in_2015,
+          :is_pending_dealer,
+          :email,
+          :password,
+          :password_confirmation,
+          :contact,
+          :city,
+          :state,
+          :zip_code
+        )
     end
+
     devise_parameter_sanitizer
       .for(:account_update) do |u|
-
-      u.permit(
-        :is_attending_campout_in_2015,
-        :is_pending_dealer,
-        :email,
-        :password,
-        :password_confirmation,
-        :current_password,
-        :contact,
-        :city,
-        :state,
-        :zip_code
-      )
+        u.permit(
+          :is_attending_campout_in_2015,
+          :is_pending_dealer,
+          :email,
+          :password,
+          :password_confirmation,
+          :current_password,
+          :contact,
+          :city,
+          :state,
+          :zip_code
+        )
     end
+
   end
 
 end
