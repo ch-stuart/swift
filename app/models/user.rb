74154d1a010ee985196a072229db564f969ed2dc
Charles Stuart <charles.stuart@gmail.com>
2015-03-17 21:13:10 -0600

    whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,12 @@ class User < ActiveRecord::Base
   def address
     address = []
 
-    address.push(self.line1) if self.line1.present?
-    address.push(self.line2) if self.line2.present?
-    address.push(self.city) if self.city.present?
-    address.push(self.state) if self.state.present?
+    address.push(self.line1)    if self.line1.present?
+    address.push(self.line2)    if self.line2.present?
+    address.push(self.city)     if self.city.present?
+    address.push(self.state)    if self.state.present?
     address.push(self.zip_code) if self.zip_code.present?
-    address.push(self.country) if self.country.present?
+    address.push(self.country)  if self.country.present?
 
     address.join(", ")
   end
