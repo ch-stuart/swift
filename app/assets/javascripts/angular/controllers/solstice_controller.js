57bc2a9c0563994a4952c0d0c57dd299cee71fe0
Charles Stuart <charles.stuart@gmail.com>
2015-04-05 17:14:52 -0600

    adjust map positioning so australians and NZ'ers show up on desktop

--- app/assets/javascripts/angular/controllers/solstice_controller.js
+++ app/assets/javascripts/angular/controllers/solstice_controller.js
@@ -12,27 +12,39 @@ SwiftApp.controller('SolsticeCtrl', [
       initializeWelcome;
 
   initializeMap = function() {
-    var tilesDict = {
-        openstreetmap: {
-            url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            options: {
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }
-        },
-        stamenmap: {
-            url: "http://tile.stamen.com/watercolor/{z}/{x}/{y}.png",
-            options: {
-                attribution: "<a target=_top href=http://maps.stamen.com>Map tiles</a> by <a target=_top href=//stamen.com>Stamen Design</a>, under <a target=_top href=http://creativecommons.org/licenses/by/3.0>CC BY 3.0</a>. Data by <a target=_top href=http://openstreetmap.org>OpenStreetMap</a>, under <a target=_top href=http://creativecommons.org/licenses/by-sa/3.0>CC BY SA</a>."
-            }
+      var mapCenter,
+          tilesDict = {
+          openstreetmap: {
+              url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              options: {
+                  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              }
+          },
+          stamenmap: {
+              url: "http://tile.stamen.com/watercolor/{z}/{x}/{y}.png",
+              options: {
+                  attribution: "<a target=_top href=http://maps.stamen.com>Map tiles</a> by <a target=_top href=//stamen.com>Stamen Design</a>, under <a target=_top href=http://creativecommons.org/licenses/by/3.0>CC BY 3.0</a>. Data by <a target=_top href=http://openstreetmap.org>OpenStreetMap</a>, under <a target=_top href=http://creativecommons.org/licenses/by-sa/3.0>CC BY SA</a>."
+              }
+          }
+      };
+
+
+    if (UA.isMobile()) {
+        mapCenter = {
+            lat: 42,
+            lng: -83,
+            zoom: 2
         }
-    };
+    } else {
+        mapCenter = {
+            lat: 14,
+            lng: -14,
+            zoom: 2
+        }
+    }
 
     angular.extend($scope, {
-        kc: {
-            lat: 39.0997266,
-            lng: -94.5785667,
-            zoom: 2
-        },
+        kc: mapCenter,
         tiles: tilesDict.stamenmap,
         defaults: {
             scrollWheelZoom: false
