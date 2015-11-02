2c840dbbb290d7e43c1c3b159e0fecad0a18851c
Charles Stuart <charles.stuart@gmail.com>
2015-11-02 07:00:26 -0700

    don't want this anymore

--- config/initializers/redirects.rb
+++ config/initializers/redirects.rb
@@ -1,15 +1,15 @@
-SwiftSite::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-
-    if Rails.env == "production"
-
-        # Redirect to https if scheme is http
-        # r301 %r{.*}, "https://www.builtbyswift.com$&", :scheme => "http"
-
-        # Redirect to www if server name is builtbyswift.com
-        r301 %r{.*}, "https://www.builtbyswift.com$&", :if => Proc.new { |rack_env|
-            rack_env["SERVER_NAME"] != "www.builtbyswift.com"
-        }
-
-    end
-
-end
+# SwiftSite::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+#
+#     if Rails.env == "production"
+#
+#         # Redirect to https if scheme is http
+#         # r301 %r{.*}, "https://www.builtbyswift.com$&", :scheme => "http"
+#
+#         # Redirect to www if server name is builtbyswift.com
+#         r301 %r{.*}, "https://www.builtbyswift.com$&", :if => Proc.new { |rack_env|
+#             rack_env["SERVER_NAME"] != "www.builtbyswift.com"
+#         }
+#
+#     end
+#
+# end
